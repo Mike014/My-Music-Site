d0d4618b441c071092aaf721e1e491a8538cea29
MikyMikeMusic <156812735+MikyMikeMusic@users.noreply.github.com>
2024-09-29 15:03:31 +0200

    Add other files

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/music')
+def music():
+    return render_template('music.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/Music/<path:filename>')
+def music_files(filename):
+    return send_from_directory(os.path.join(app.root_path, 'Music'), filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
